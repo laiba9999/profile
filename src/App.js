9a797434d9a47f350f19f68f8449ce4aad573131
laiba9999 <laibakirmani@hotmail.com>
2021-10-28 21:38:35 +0100

    "commit again"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Laiba Website </h1>
-      <h2>wel</h2>
+      <h2>welco</h2>
     </div>
   );
 }
